750ad6a7fad847ce152db627953fff0347f3c648
Thamires Ramos <thamiresramos014@gmail.com>
2021-08-12 18:13:19 -0300

    adicionando novos acessos para os não autenticados

--- src/main/java/org/serratec/resources/LeitorResource.java
+++ src/main/java/org/serratec/resources/LeitorResource.java
@@ -15,7 +15,6 @@ import org.serratec.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
